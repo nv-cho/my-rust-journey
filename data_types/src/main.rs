a570de56a7c6af0ea33f389963066d640775bd7e
Ignacio Presas <cu.ouza@gmail.com>
2023-03-21 15:07:37 -0300

    add: data types

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -27,4 +27,21 @@ fn main() {
     /* El tipo de dato "char" (caracter) en Rust tiene un tamaño de 4 bytes y estan alineados al "Unicode Scalar Value", lo que significa que pueden representar mas que ASCII. */
     let z = '🔧';
     println!("The value of c is: {z}");
+
+    /*
+     * Compuestos:
+     * Tuples: tuplas
+     * Arrays: arreglos
+     */
+
+    /* Las tuplas son un tipo de dato que permite agrupar valores de diferentes tipos en una sola estructura. */
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    /* Para acceder a los elementos individuales de la tupla podemos desestructurar creando distintas variables para los distintos valores */
+    let (x, y, z) = tup;
+    println!("The value of x, y, z is: {x} {y} {z}");
+
+    /* Tambien podemos acceder a sus valores mediante puntos */
+    let five_hundred = tup.0;
+    println!("The value of five_hundred is: {five_hundred}");
 }
